c2fe447ea46080f7412d1a78e25f600f57548325
parinchaipunya <parin.cha@kmutt.ac.th>
2025-07-12 20:47:07 +0700

    Files changed:   > A  zsh_setup.sh   > commited by parinchaipunya

---
+++ zsh_setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : parin (parin@$HOSTNAME)
+# @file        : zsh_setup
+# @created     : Saturday Jul 12, 2025 20:41:11 +07
+#
+# @description : 
+######################################################################
+
+echo "Installing Oh-My-Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "Installing zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
