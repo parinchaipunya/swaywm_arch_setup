31c8eb87268fca40aed8c9b997c698c429cdc76e
parinchaipunya <parin.cha@kmutt.ac.th>
2025-07-12 20:49:32 +0700

    Files changed:   > M  zsh_setup.sh   > commited by parinchaipunya

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -8,9 +8,10 @@
 # @description : 
 ######################################################################
 
-echo "Installing Oh-My-Zsh..."
+echo "::: Installing Oh-My-Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Installing zsh-autosuggestions"
+echo "::: Installing zsh-autosuggestions..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo "Installing zsh-syntax-highlighting"
+echo "::: Installing zsh-syntax-highlighting..."
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "::: Installing power-level-10k..."
