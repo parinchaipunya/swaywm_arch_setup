a35d442b82ca81d5923938cb68433bd0bc5ad46a
parinchaipunya <parin.cha@kmutt.ac.th>
2025-07-12 21:03:22 +0700

    Files changed:   > A  latex_setup.sh A  latexpack.txt R  paru-install.sh -> paru_install.sh   > commited by parinchaipunya

---
+++ latex_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : parin (parin@$HOSTNAME)
+# @file        : latex_setup
+# @created     : Saturday Jul 12, 2025 20:58:31 +07
+#
+# @description : 
+######################################################################
+
+paru -S --noconfirm $(< latexpack.txt)
