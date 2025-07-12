127309f539e8ec82555e2b715fcfcf85c39d4556
parinchaipunya <parin.cha@kmutt.ac.th>
2025-07-12 20:52:03 +0700

    Files changed:   > M  zsh_setup.sh   > commited by parinchaipunya

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -15,3 +15,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 echo "::: Installing zsh-syntax-highlighting..."
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo "::: Installing power-level-10k..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
