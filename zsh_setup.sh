de7a9a795593baf53d6820376691e72b80c2dcfa
parinchaipunya <parin.cha@kmutt.ac.th>
2025-07-14 14:42:23 +0700

    Files changed:   > M  zsh_setup.sh   > commited by parinchaipunya

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -16,3 +16,5 @@ echo "::: Installing zsh-syntax-highlighting..."
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo "::: Installing power-level-10k..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+echo "::: Installing TPM: tmux plugin manager..."
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
